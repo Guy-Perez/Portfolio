3f48357e3144a33d96442117912ba7a41f7f273b
Guy <perezguy@live.fr>
2022-02-18 16:51:50 +0100

    Guy: Finish HOMe + ABOUT

---
+++ js/text-animation.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $(".text").typed({
+        strings: [
+            "<strong class='primary'>PEREZ GUY</strong>", 
+            "<strong>i love coding</strong>",
+            "in <strong class='primary'>javascript</strong>.",
+        ],
+        typeSpeed: 100,
+        loop: true
+    })
+});
